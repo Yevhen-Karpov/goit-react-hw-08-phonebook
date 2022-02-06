9616c8dea95e35920f40df7c00420aca9dd9db66
Yevhen-Karpov <goldblogart@gmail.com>
2022-02-06 13:38:51 +0200

    1

---
+++ build/static/js/8.088a486c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[8],{241:function(t,e,n){"use strict";n.r(e);n(0);var i=n(3),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:108,textAlign:"center",color:"#154584"}};e.default=function(){return Object(i.jsx)("div",{style:c.container,children:Object(i.jsx)("h1",{style:c.title,children:"WELCOM TO PHONEBOOK"})})}}}]);
+//# sourceMappingURL=8.088a486c.chunk.js.map
